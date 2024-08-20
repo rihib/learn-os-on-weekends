b3bbcf93f10bc1dde1c7294838e07e2200c8f0c1
rihib <rihib@rihib.dev>
2024-08-20 23:16:44 +0900

    Day1

--- os/kernel.h
+++ os/kernel.h
@@ -6,3 +6,16 @@
     while (1) {                                                           \
     }                                                                     \
   } while (0)
+
+#define READ_CSR(reg)                                     \
+  ({                                                      \
+    unsigned long __tmp;                                  \
+    __asm__ __volatile__("csrr %0, " #reg : "=r"(__tmp)); \
+    __tmp;                                                \
+  })
+
+#define WRITE_CSR(reg, value)                               \
+  do {                                                      \
+    uint32_t __tmp = (value);                               \
+    __asm__ __volatile__("csrw " #reg ", %0" ::"r"(__tmp)); \
+  } while (0)

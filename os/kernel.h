9bd9905421599d99bdc5dc83602111e4d5d35a00
rihib <rihib@rihib.dev>
2024-08-21 20:43:02 +0900

    多段ページテーブルの実装

--- os/kernel.h
+++ os/kernel.h
@@ -1,8 +1,14 @@
 #pragma once
 
-#define PROCS_MAX 8      // 最大プロセス数
-#define PROC_UNUSED 0    // 未使用のプロセス管理構造体
-#define PROC_RUNNABLE 1  // 実行可能なプロセス
+#define PROCS_MAX 8           // 最大プロセス数
+#define PROC_UNUSED 0         // 未使用のプロセス管理構造体
+#define PROC_RUNNABLE 1       // 実行可能なプロセス
+#define SATP_SV32 (1u << 31)  // Sv32モードでページングを有効化
+#define PAGE_V (1 << 0)       // 有効化ビット
+#define PAGE_R (1 << 1)       // 読み込み可能
+#define PAGE_W (1 << 2)       // 書き込み可能
+#define PAGE_X (1 << 3)       // 実行可能
+#define PAGE_U (1 << 4)       // ユーザーモードでアクセス可能
 
 #define PANIC(fmt, ...)                                                   \
   do {                                                                    \

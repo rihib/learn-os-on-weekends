636ffb7148c1e65bb4f3e26c827e62612957b2dc
guredora <contact@guredora.com>
2024-08-26 21:45:31 +0900

    use stack

--- os/kernel.c
+++ os/kernel.c
@@ -7,88 +7,84 @@ extern char __stack_top[];
 extern char __bss[], __bss_end[];
 extern char __free_ram[], __free_ram_end[];
 extern char _binary_shell_bin_start[], _binary_shell_bin_size[];
-struct trap_frame trapflame;
+
 __attribute__((aligned(4))) void traphandle(void){
   // save registers
   __asm__ __volatile__ (
-    "csrw sscratch, a0\n"
-    "la a0, trapflame\n"
-    "sw ra, 0(a0)\n"
-    "sw gp, 4(a0)\n"
-    "sw tp, 8(a0)\n"
-    "sw t0, 12(a0)\n"
-    "sw t1, 16(a0)\n"
-    "sw t2, 20(a0)\n"
-    "sw t3, 24(a0)\n"
-    "sw t4, 28(a0)\n"
-    "sw t5, 32(a0)\n"
-    "sw t6, 36(a0)\n"
-    // "sw a0, 40(a0)\n" // a0 is used as a pointer to trapflame
-    "sw a1, 44(a0)\n"
-    "sw a2, 48(a0)\n"
-    "sw a3, 52(a0)\n"
-    "sw a4, 56(a0)\n"
-    "sw a5, 60(a0)\n"
-    "sw a6, 64(a0)\n"
-    "sw a7, 68(a0)\n"
-    "sw s0, 72(a0)\n"
-    "sw s1, 76(a0)\n"
-    "sw s2, 80(a0)\n"
-    "sw s3, 84(a0)\n"
-    "sw s4, 88(a0)\n"
-    "sw s5, 92(a0)\n"
-    "sw s6, 96(a0)\n"
-    "sw s7, 100(a0)\n"
-    "sw s8, 104(a0)\n"
-    "sw s9, 108(a0)\n"
-    "sw s10, 112(a0)\n"
-    "sw s11, 116(a0)\n"
-    "sw sp, 120(a0)\n"
+    "csrw sscratch, sp\n"
+    "addi sp, sp, -124\n"
+    "sw ra, 0(sp)\n"
+    "sw gp, 4(sp)\n"
+    "sw tp, 8(sp)\n"
+    "sw t0, 12(sp)\n"
+    "sw t1, 16(sp)\n"
+    "sw t2, 20(sp)\n"
+    "sw t3, 24(sp)\n"
+    "sw t4, 28(sp)\n"
+    "sw t5, 32(sp)\n"
+    "sw t6, 36(sp)\n"
+    "sw a0, 40(sp)\n"
+    "sw a1, 44(sp)\n"
+    "sw a2, 48(sp)\n"
+    "sw a3, 52(sp)\n"
+    "sw a4, 56(sp)\n"
+    "sw a5, 60(sp)\n"
+    "sw a6, 64(sp)\n"
+    "sw a7, 68(sp)\n"
+    "sw s0, 72(sp)\n"
+    "sw s1, 76(sp)\n"
+    "sw s2, 80(sp)\n"
+    "sw s3, 84(sp)\n"
+    "sw s4, 88(sp)\n"
+    "sw s5, 92(sp)\n"
+    "sw s6, 96(sp)\n"
+    "sw s7, 100(sp)\n"
+    "sw s8, 104(sp)\n"
+    "sw s9, 108(sp)\n"
+    "sw s10, 112(sp)\n"
+    "sw s11, 116(sp)\n"
     : 
     : 
     : "memory"
   );
-  trapflame.a0 = READ_CSR(sscratch);
   printf("scause: %x\n", READ_CSR(SCAUSE));
   printf("sepc: %x\n", READ_CSR(SEPC));
   printf("stval: %x\n", READ_CSR(STVAL));
   PANIC("trap!");
   // restore registers
-  WRITE_CSR(sscratch, trapflame.a0);
   __asm__ __volatile__ (
-    "la a0, trapflame\n"
-    "lw ra, a0\n"
-    "lw gp, 4(a0)\n"
-    "lw tp, 8(a0)\n"
-    "lw t0, 12(a0)\n"
-    "lw t1, 16(a0)\n"
-    "lw t2, 20(a0)\n"
-    "lw t3, 24(a0)\n"
-    "lw t4, 28(a0)\n"
-    "lw t5, 32(a0)\n"
-    "lw t6, 36(a0)\n"
-    // "lw a0, 40(a0)\n" // a0 is used as a pointer to trapflame
-    "lw a1, 44(a0)\n"
-    "lw a2, 48(a0)\n"
-    "lw a3, 52(a0)\n"
-    "lw a4, 56(a0)\n"
-    "lw a5, 60(a0)\n"
-    "lw a6, 64(a0)\n"
-    "lw a7, 68(a0)\n"
-    "lw s0, 72(a0)\n"
-    "lw s1, 76(a0)\n"
-    "lw s2, 80(a0)\n"
-    "lw s3, 84(a0)\n"
-    "lw s4, 88(a0)\n"
-    "lw s5, 92(a0)\n"
-    "lw s6, 96(a0)\n"
-    "lw s7, 100(a0)\n"
-    "lw s8, 104(a0)\n"
-    "lw s9, 108(a0)\n"
-    "lw s10, 112(a0)\n"
-    "lw s11, 116(a0)\n"
-    "lw sp, 120(a0)\n"
-    "csrr a0, sscratch\n"
+    "csrr sp, sscratch\n"
+    "lw ra, 0(sp)\n"
+    "lw gp, 4(sp)\n"
+    "lw tp, 8(sp)\n"
+    "lw t0, 12(sp)\n"
+    "lw t1, 16(sp)\n"
+    "lw t2, 20(sp)\n"
+    "lw t3, 24(sp)\n"
+    "lw t4, 28(sp)\n"
+    "lw t5, 32(sp)\n"
+    "lw t6, 36(sp)\n"
+    "lw a0, 40(sp)\n"
+    "lw a1, 44(sp)\n"
+    "lw a2, 48(sp)\n"
+    "lw a3, 52(sp)\n"
+    "lw a4, 56(sp)\n"
+    "lw a5, 60(sp)\n"
+    "lw a6, 64(sp)\n"
+    "lw a7, 68(sp)\n"
+    "lw s0, 72(sp)\n"
+    "lw s1, 76(sp)\n"
+    "lw s2, 80(sp)\n"
+    "lw s3, 84(sp)\n"
+    "lw s4, 88(sp)\n"
+    "lw s5, 92(sp)\n"
+    "lw s6, 96(sp)\n"
+    "lw s7, 100(sp)\n"
+    "lw s8, 104(sp)\n"
+    "lw s9, 108(sp)\n"
+    "lw s10, 112(sp)\n"
+    "lw s11, 116(sp)\n"
+    "addi sp, sp, 124"
     : 
   );
   // return from trap
@@ -120,7 +116,7 @@ __attribute__((section(".text.boot"))) __attribute__((naked)) void boot(void) {
 
 struct sbiret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4,
                        long arg5, long fid, long eid) {
-  register long a0 __asm__("a0") = arg0;
+  register long sp __asm__("sp") = arg0;
   register long a1 __asm__("a1") = arg1;
   register long a2 __asm__("a2") = arg2;
   register long a3 __asm__("a3") = arg3;
@@ -130,11 +126,11 @@ struct sbiret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4,
   register long a7 __asm__("a7") = eid;
 
   __asm__ __volatile__("ecall"
-                       : "=r"(a0), "=r"(a1)
-                       : "r"(a0), "r"(a1), "r"(a2), "r"(a3), "r"(a4), "r"(a5),
+                       : "=r"(sp), "=r"(a1)
+                       : "r"(sp), "r"(a1), "r"(a2), "r"(a3), "r"(a4), "r"(a5),
                          "r"(a6), "r"(a7)
                        : "memory");
-  return (struct sbiret){.error = a0, .value = a1};
+  return (struct sbiret){.error = sp, .value = a1};
 }
 
 void putchar(char ch) {

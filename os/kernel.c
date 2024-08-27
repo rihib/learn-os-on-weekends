a9c563965d2aac90fc045a037860cf3a83868327
yunative <47847960+yi7242@users.noreply.github.com>
2024-08-27 12:35:21 +0900

    スタックポインタの値を保存

--- os/kernel.c
+++ os/kernel.c
@@ -25,7 +25,43 @@ __attribute__((section(".text.boot"))) __attribute__((naked)) void boot(void) {
 }
 
 void trap(void) {
+  // WRITE_CSR(sscratch, sp);
+  __asm__ __volatile__(
+    "csrw sscratch, sp;\n"
+    "addi sp, sp, -120;\n"
+    "sw ra, 0(sp);\n"
+    "sw gp, -4(sp);\n"
+    "sw tp, -8(sp);\n"
+    "sw t0, -12(sp);\n"
+    "sw t1, -16(sp);\n"
+    "sw t2, -20(sp);\n"
+    "sw t3, -24(sp);\n"
+    "sw t4, -28(sp);\n"
+    "sw t5, -32(sp);\n"
+    "sw t6, -36(sp);\n"
+    "sw a0, -40(sp);\n"
+    "sw a1, -44(sp);\n"
+    "sw a2, -48(sp);\n"
+    "sw a3, -52(sp);\n"
+    "sw a4, -56(sp);\n"
+    "sw a5, -60(sp);\n"
+    "sw a6, -64(sp);\n"
+    "sw a7, -68(sp);\n"
+    "sw s0, -72(sp);\n"
+    "sw s1, -76(sp);\n"
+    "sw s2, -80(sp);\n"
+    "sw s3, -84(sp);\n"
+    "sw s4, -88(sp);\n"
+    "sw s5, -92(sp);\n"
+    "sw s6, -96(sp);\n"
+    "sw s7, -100(sp);\n"
+    "sw s8, -104(sp);\n"
+    "sw s9, -108(sp);\n"
+    "sw s10, -112(sp);\n"
+    "sw s11, -116(sp);\n"
+  );
   PANIC("trap!");
+  
 }
 
 struct sbiret sbi_call(long arg0, long arg1, long arg2, long arg3, long arg4,
